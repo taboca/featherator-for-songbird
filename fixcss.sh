c785a048f92753ad94a35c4e420b4b04861ec444
root <taboca@gmail.com>
2009-02-25 16:44:38 -0300

    Some images main menu, nav bar, sed regexp fixes to colors...

--- fixcss.sh
+++ fixcss.sh
@@ -1,20 +1,43 @@
 
 echo "============================================="
-echo "Table - 255,230,255 - high up "
-echo "Table - 255,210,250 - high up "
-echo "Table - 250,190,240 - high "
-echo "Table - 225,170,215 - high 10% "
-echo "Table - 200,150,195 - high 20% "
-echo "Table - 125,90,120  - medium "
-echo "Table - 100,70,95   - dark "
-
+echo "245,245,245 -  tabbox, and a lot of panels "
+echo "255,255,255 - tabpanels "
+echo "220,220,220 - tabs "
 
 
 echo "=============== general all ================"
 
-sed -e "s/#eaeaea/rgb(250,190,240)/g" ./build-chrome-classic/chrome/classic/skin/classic/global/global.css > ./temp/temp.css
+sed -e "s/rgb(255,255,255)/rgb(255,210,250)/g" ./output-build-chrome/chrome/skin/feathers.css > ./temp/temp.css
+cp ./temp/temp.css ./output-build-chrome/chrome/skin/feathers.css
 
-cp ./temp/temp.css ./build-chrome-classic/chrome/classic/skin/classic/global/global.css
+sed -e "s/rgb(250,250,250)/rgb(255,200,245)/g" ./output-build-chrome/chrome/skin/feathers.css > ./temp/temp.css
+cp ./temp/temp.css ./output-build-chrome/chrome/skin/feathers.css
+
+sed -e "s/rgb(245,245,245)/rgb(250,190,240)/g" ./output-build-chrome/chrome/skin/feathers.css > ./temp/temp.css
+cp ./temp/temp.css ./output-build-chrome/chrome/skin/feathers.css
+
+sed -e "s/rgb(235,235,235)/rgb(245,185,235)/g" ./output-build-chrome/chrome/skin/feathers.css > ./temp/temp.css
+cp ./temp/temp.css ./output-build-chrome/chrome/skin/feathers.css
+
+sed -e "s/rgb(220,220,220)/rgb(240,180,230)/g" ./output-build-chrome/chrome/skin/feathers.css > ./temp/temp.css
+cp ./temp/temp.css ./output-build-chrome/chrome/skin/feathers.css
+
+sed -e "s/rgb(215,215,215)/rgb(230,170,220)/g" ./output-build-chrome/chrome/skin/feathers.css > ./temp/temp.css
+cp ./temp/temp.css ./output-build-chrome/chrome/skin/feathers.css
+
+sed -e "s/rgb(161,161,161)/rgb(200,140,190)/g" ./output-build-chrome/chrome/skin/feathers.css > ./temp/temp.css
+cp ./temp/temp.css ./output-build-chrome/chrome/skin/feathers.css
+
+sed -e "s/rgb(140,140,140)/rgb(195,135,185)/g" ./output-build-chrome/chrome/skin/feathers.css > ./temp/temp.css
+cp ./temp/temp.css ./output-build-chrome/chrome/skin/feathers.css
+
+sed -e "s/rgb(131,131,131)/rgb(190,130,180)/g" ./output-build-chrome/chrome/skin/feathers.css > ./temp/temp.css
+cp ./temp/temp.css ./output-build-chrome/chrome/skin/feathers.css
+
+
+sed -e "s/rgb(80,80,80)/rgb(110,40,90)/g" ./output-build-chrome/chrome/skin/feathers.css > ./temp/temp.css
+cp ./temp/temp.css ./output-build-chrome/chrome/skin/feathers.css
+
 
 
 
