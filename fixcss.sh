61efee50721e168cc47f76ef048dc03a034559fa
root <taboca@gmail.com>
2009-03-02 00:44:41 -0300

    Fix to the huge 1500x300(w) image for bg button. Fixes to the post-fix fixcss - more colors; fixed menupopup background with a patch and new svg with vertical shade; 1px solid white to the top menu popup menus; some other stuff

--- fixcss.sh
+++ fixcss.sh
@@ -17,6 +17,9 @@ cp ./temp/temp.css ./output-build-chrome/chrome/skin/feathers.css
 cat  ./output-build-chrome/chrome/skin/colors.css ./patches/colors.css > ./temp/temp.css
 cp ./temp/temp.css ./output-build-chrome/chrome/skin/colors.css
 
+cat  ./output-build-chrome/chrome/skin/images.css ./patches/images.css > ./temp/temp.css
+cp ./temp/temp.css ./output-build-chrome/chrome/skin/images.css
+
 echo "=============== general all ================"
 
 
@@ -30,13 +33,23 @@ cp ./temp/temp.css ./output-build-chrome/chrome/skin/feathers.css
 sed -e "s/rgb(245,245,245)/rgb(250,190,240)/g" ./output-build-chrome/chrome/skin/feathers.css > ./temp/temp.css
 cp ./temp/temp.css ./output-build-chrome/chrome/skin/feathers.css
 
-sed -e "s/rgb(235,235,235)/rgb(245,185,235)/g" ./output-build-chrome/chrome/skin/feathers.css > ./temp/temp.css
+sed -e "s/rgb(235,235,235)/rgb(235,175,225)/g" ./output-build-chrome/chrome/skin/feathers.css > ./temp/temp.css
+cp ./temp/temp.css ./output-build-chrome/chrome/skin/feathers.css
+
+sed -e "s/rgb(225,225,225)/rgb(245,185,235)/g" ./output-build-chrome/chrome/skin/feathers.css > ./temp/temp.css
 cp ./temp/temp.css ./output-build-chrome/chrome/skin/feathers.css
 
 sed -e "s/rgb(220,220,220)/rgb(240,180,230)/g" ./output-build-chrome/chrome/skin/feathers.css > ./temp/temp.css
 cp ./temp/temp.css ./output-build-chrome/chrome/skin/feathers.css
 
 sed -e "s/rgb(215,215,215)/rgb(230,170,220)/g" ./output-build-chrome/chrome/skin/feathers.css > ./temp/temp.css
+cp ./temp/temp.css ./output-build-chrome/chrome/skin/feathers.css
+
+sed -e "s/rgb(200,200,200)/rgb(255,240,250)/g" ./output-build-chrome/chrome/skin/feathers.css > ./temp/temp.css
+cp ./temp/temp.css ./output-build-chrome/chrome/skin/feathers.css
+
+sed -e "s/rgb(180,180,180)/rgb(240,224,235)/g" ./output-build-chrome/chrome/skin/feathers.css > ./temp/temp.css
+sed -e "s/rgb(180,180,180)/rgb(255,255,255)/g" ./output-build-chrome/chrome/skin/feathers.css > ./temp/temp.css
 cp ./temp/temp.css ./output-build-chrome/chrome/skin/feathers.css
 
 sed -e "s/rgb(161,161,161)/rgb(200,140,190)/g" ./output-build-chrome/chrome/skin/feathers.css > ./temp/temp.css
@@ -46,6 +59,9 @@ sed -e "s/rgb(140,140,140)/rgb(195,135,185)/g" ./output-build-chrome/chrome/skin
 cp ./temp/temp.css ./output-build-chrome/chrome/skin/feathers.css
 
 sed -e "s/rgb(131,131,131)/rgb(190,130,180)/g" ./output-build-chrome/chrome/skin/feathers.css > ./temp/temp.css
+cp ./temp/temp.css ./output-build-chrome/chrome/skin/feathers.css
+
+sed -e "s/rgb(101,101,101)/rgb(170,120,160)/g" ./output-build-chrome/chrome/skin/feathers.css > ./temp/temp.css
 cp ./temp/temp.css ./output-build-chrome/chrome/skin/feathers.css
 
 
@@ -62,3 +78,10 @@ cp ./temp/temp.css ./output-build-chrome/chrome/skin/feathers.css
 
 
 
+ echo "others menu items "
+
+sed -e "s/rgb(169,212,255)/rgb(220,120,190)/g" ./output-build-chrome/chrome/skin/feathers.css > ./temp/temp.css
+cp ./temp/temp.css ./output-build-chrome/chrome/skin/feathers.css
+
+sed -e "s/rgb(164,207,250)/rgb(200,100,170)/g" ./output-build-chrome/chrome/skin/feathers.css > ./temp/temp.css
+cp ./temp/temp.css ./output-build-chrome/chrome/skin/feathers.css
